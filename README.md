a0674d0d9ba200d421c53921180f00f560ece6b2
= <zekriad@gmail.com>
2016-05-15 01:17:35 -0500

    Starting somewhere.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ignition
+
+Ignition is a polyglot Vagrantfile generator.
+
+Clone, build, throw it on your path.
+
+```
+ignition postgres haskell
+```
+
+Peruse your shiny new Vagrantfile in your current directory. Currently `base postgres haskell elixir java clojure ruby node elm` are available, with base installed by default. Multiple may be specified. Running on windows assumes Hyper-V and SMB, virtualbox otherwise.
+
+Parsing args for things like box and memory are on the TODO list, as is removing the reliance on Ruby in the Vagrantfile.
---
+++ src/Lib.hs
@@ -0,0 +1,42 @@
+module Lib
+    ( entry
+    ) where
+
+import System.Environment (getArgs)
+import Network.Wreq
+import Control.Lens((^.))
+import qualified Data.ByteString.Lazy.Char8 as BLC (unpack)
+import Data.Monoid ((<>))
+import Control.Monad (when)
+import Control.Monad.IO.Class (liftIO)
+
+entry :: IO ()
+entry = do
+    response <- get "https://raw.githubusercontent.com/zekriad/ignition/master/template/Vagrantfile"
+    stacks   <- stackString
+
+    let template    = response ^. responseBody
+        vagrantfile = stackLine stacks <> BLC.unpack template
+
+    writeFile "./Vagrantfile" vagrantfile
+
+supportedStacks :: [String]
+supportedStacks = ["base", "postgres", "haskell", "elixir", "java", "clojure", "ruby", "node", "elm"]
+
+unsupportedStack :: String -> Bool
+unsupportedStack stack = stack `notElem` supportedStacks
+
+stackString :: IO String
+stackString = do
+    stackArgs <- getArgs
+
+    when (any unsupportedStack stackArgs) $
+        liftIO $ fail unsupportedMessage
+
+    return $ unwords stackArgs
+
+  where
+    unsupportedMessage = ("Supported stacks: " <> unwords supportedStacks)
+
+stackLine :: String -> String
+stackLine stacks = "PLATFORMS = %i(" <> stacks <> ")\n\n"
